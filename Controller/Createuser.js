3fc5103eb212658688a09757a10dfe205e71d190
Riya-dhingra3 <97179871+Riya-dhingra3@users.noreply.github.com>
2023-11-29 04:02:10 -0800

    first commit

--- Controller/Createuser.js
+++ Controller/Createuser.js
@@ -1,18 +1,9 @@
 const Usermodel=require("../Model/PostSchema");
-<<<<<<< HEAD
 const comment=require("../Model/CommentSchema");
-exports.create=async(req,res)=>{
-    try{
-    const data=req.body;
-=======
-const Comment=require("../Model/CommentSchema");
 
 exports.create=async(req,res)=>{
     try{
     const data=req.body;
-    const {author}=req.body
-
->>>>>>> e115d42cb6574818acd4acf93f728f9824d9f996
     const dataa=await Usermodel.create(data);
     
     res.status(200).json({
@@ -21,7 +12,6 @@ exports.create=async(req,res)=>{
         message:"data updated successfully"
             })
         }
-<<<<<<< HEAD
     catch(err){
         res.status(500).json({
             success:false,
@@ -35,29 +25,6 @@ exports.createc=async(req,res)=>{
 try{
     const data=req.body;
     const dataa=await comment.create(data);
-    
-    res.status(200).json({
-        success:true,
-        data:dataa,
-        message:"data updated successfully"
-            })
-        }
-=======
-
->>>>>>> e115d42cb6574818acd4acf93f728f9824d9f996
-    catch(err){
-        res.status(500).json({
-            success:false,
-            message:"Some error",
-            err: err.message
-        })
-    }
-}
-
-    exports.createc=async(req,res)=>{
-    try{
-    const data=req.body;
-    const dataa=await Comment.create(data);
     
     res.status(200).json({
         success:true,
