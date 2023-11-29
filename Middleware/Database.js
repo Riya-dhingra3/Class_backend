791df0460a79c828ef2c6a45a55d0f296fa3a6a8
Riya-dhingra3 <97179871+Riya-dhingra3@users.noreply.github.com>
2023-11-29 04:06:38 -0800

    first commit

--- Middleware/Database.js
+++ Middleware/Database.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
 
+
 mongoose.connect("mongodb+srv://riyadhingra248:Riya1234@cluster0.fcu50nr.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>console.log("Database Connected Successfully"))
 .catch((err)=>console.log(err));
