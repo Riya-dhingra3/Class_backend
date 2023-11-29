e115d42cb6574818acd4acf93f728f9824d9f996
Riya-dhingra3 <97179871+Riya-dhingra3@users.noreply.github.com>
2023-11-29 03:45:17 -0800

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,14 +1,18 @@
 const express=require("express");
 const router=express.Router();
 
-const {create}=require("../Controller/Createuser");
-const {getbyid}=require("../Controller/Getuser");
-const {update}=require("../Controller/Updateuser");
-const {deleteu}=require("../Controller/Deleteuser");
+const {create,createc}=require("../Controller/Createuser");
+const {getbyid,getbyidc}=require("../Controller/Getuser");
+const {update,updatec}=require("../Controller/Updateuser");
+const {deleteu,deleteuc}=require("../Controller/Deleteuser");
 
 router.post("/posts",create);
-router.get("/post/:id",getbyid);
+router.post("/posts/:id/comments",createc);
+router.get("/posts/:id",getbyid);
+router.get("/posts/:id/comments",getbyidc);
 router.post("/post/:id",update);
+router.post("/posts/:id/comments/:id",updatec);
 router.delete("/post/:id",deleteu);
+router.delete("/posts/:id/comments/:id,deletec);
 
 module.exports=router;
