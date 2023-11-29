85cfe9572ff29d276c636c44706ab74bc31d07a8
Riya-dhingra3 <97179871+Riya-dhingra3@users.noreply.github.com>
2023-11-29 04:04:54 -0800

    first commit

--- Controller/Updateuser.js
+++ Controller/Updateuser.js
@@ -1,15 +1,8 @@
 const Usermodel=require("../Model/PostSchema");
 const comment=require("../Model/CommentSchema");
-<<<<<<< HEAD
-
-=======
->>>>>>> e115d42cb6574818acd4acf93f728f9824d9f996
-
 exports.update=async(req,res)=>{
     try{
         const {id}=req.params;
-<<<<<<< HEAD
-        
         const data=await Usermodel.findByIdAndUpdate(
             {_id:id}
         )
@@ -33,29 +26,7 @@ exports.updatec=async(req,res)=>{
         const {id}=req.params;
         
         const data=await comment.findByIdAndUpdate(
-=======
         const data=await Usermodel.findByIdAndUpdate(
->>>>>>> e115d42cb6574818acd4acf93f728f9824d9f996
-            {_id:id})
-
-        res.status(200).json({
-            success:true,
-            data:data,
-            message:"Data updated Successfully"
-        })
-    }
-    catch(err){
-        res.status(500).json({
-            success:false,
-            message:err
-        })
-    }
-}
-exports.updatec=async(req,res)=>{
-    try{
-        const {id}=req.params;
-        
-        const data=await comment.findByIdAndUpdate(
             {_id:id})
 
         res.status(200).json({
