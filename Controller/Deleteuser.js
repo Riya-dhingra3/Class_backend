8e3206ceb2f9b8e5bc4e21b420bd9f0f442ee075
Riya-dhingra3 <97179871+Riya-dhingra3@users.noreply.github.com>
2023-11-29 03:39:34 -0800

    Update Deleteuser.js

--- Controller/Deleteuser.js
+++ Controller/Deleteuser.js
@@ -1,9 +1,29 @@
 const Usermodel=require("../Model/PostSchema");
+const Comment=require("../Model/CommentSchema");
 
 exports.deleteu=async(req,res)=>{
     try{
         const {id}=req.params;
         const ddata=await Usermodel.findByIdAndDelete(id);
+
+        res.status(200).json({
+            success:true,
+            message:"data deleted successfully",
+            data:ddata
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:"opps some error!!"
+        })
+    }
+}
+
+exports.deleteuc=async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const ddata=await Comment.findByIdAndDelete(id);
 
         res.status(200).json({
             success:true,
