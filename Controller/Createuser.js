43b33c3589a55cf44481cd92b360abf8acd129c1
Riya-dhingra3 <97179871+Riya-dhingra3@users.noreply.github.com>
2023-11-29 03:33:11 -0800

    Update Createuser.js

--- Controller/Createuser.js
+++ Controller/Createuser.js
@@ -1,10 +1,10 @@
 const Usermodel=require("../Model/PostSchema");
-
+const Comment=require("../Model/CommentSchema");
 exports.create=async(req,res)=>{
     try{
     const data=req.body;
-    const {email}=req.body;
-    const activeuser=await Usermodel.findOne({email}).select("_id").lean();
+    const {author}=req.body
+    const activeuser=await Usermodel.findOne({author}).select("_id").lean();
     if(activeuser){
         res.status(400).json({
             success:false,
@@ -21,6 +21,25 @@ exports.create=async(req,res)=>{
             })
         }
     }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:"Some error",
+            err: err.message
+        })
+    }
+
+    exports.createc=async(req,res)=>{
+    try{
+    const data=req.body;
+    const dataa=await Comment.create(data);
+    
+    res.status(200).json({
+        success:true,
+        data:dataa,
+        message:"data updated successfully"
+            })
+        }
     catch(err){
         res.status(500).json({
             success:false,
